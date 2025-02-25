2daaca0e9bcee2496a4624e03ce8d9aadb784ff1
Ashraf Qopiah <124946844+ashrafmo-1@users.noreply.github.com>
2025-02-25 19:03:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// resolve: {
-	// 	extensions: [".js", ".jsx"],
-	// },
-	// base: "https://napco-admin.testingelmo.com/dashboard",
+	 resolve: {
+	 	extensions: [".js", ".jsx"],
+	 },
+	 base: "https://napco-admin.testingelmo.com/dashboard",
 
 	base: '/',
 	server: {
